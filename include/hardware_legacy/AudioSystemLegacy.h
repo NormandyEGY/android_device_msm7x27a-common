787c12614facdb4c1c512c60454c425abd5f6f5d
androidlover5842 <androidlover5842@gmail.com>
2017-05-31 01:35:14 +0530

    common: updates for cm13
    bootloop with lineage os bootanimation for now

--- include/hardware_legacy/AudioSystemLegacy.h
+++ include/hardware_legacy/AudioSystemLegacy.h
@@ -45,14 +45,9 @@ enum audio_source {
     AUDIO_SOURCE_CAMCORDER = 5,
     AUDIO_SOURCE_VOICE_RECOGNITION = 6,
     AUDIO_SOURCE_VOICE_COMMUNICATION = 7,
-#ifdef MTK_HARDWARE
-    AUDIO_SOURCE_FM_RX = 9,
-#endif
-#ifdef QCOM_HARDWARE
+#ifdef LEGACY_ALSA_AUDIO
     AUDIO_SOURCE_REMOTE_SUBMIX = 8,
-    AUDIO_SOURCE_FM_RX = 9,
-    AUDIO_SOURCE_FM_RX_A2DP = 10,
-    AUDIO_SOURCE_MAX = AUDIO_SOURCE_FM_RX_A2DP,
+    AUDIO_SOURCE_MAX = AUDIO_SOURCE_REMOTE_SUBMIX,
 #else
     AUDIO_SOURCE_MAX = AUDIO_SOURCE_VOICE_COMMUNICATION,
 #endif
@@ -62,7 +57,6 @@ enum audio_source {
 
 class AudioSystem {
 public:
-#if 1
     enum stream_type {
         DEFAULT          =-1,
         VOICE_CALL       = 0,
@@ -75,7 +69,7 @@ public:
         ENFORCED_AUDIBLE = 7,
         DTMF             = 8,
         TTS              = 9,
-#ifdef QCOM_HARDWARE
+#ifdef LEGACY_ALSA_AUDIO
         INCALL_MUSIC     = 10,
 #endif
         NUM_STREAM_TYPES
@@ -169,6 +163,7 @@ public:
                 CHANNEL_OUT_TOP_FRONT_LEFT | CHANNEL_OUT_TOP_FRONT_CENTER |
                 CHANNEL_OUT_TOP_FRONT_RIGHT | CHANNEL_OUT_TOP_BACK_LEFT |
                 CHANNEL_OUT_TOP_BACK_CENTER | CHANNEL_OUT_TOP_BACK_RIGHT),
+
         CHANNEL_IN_LEFT = 0x4,
         CHANNEL_IN_RIGHT = 0x8,
         CHANNEL_IN_FRONT = 0x10,
@@ -224,11 +219,10 @@ public:
         DEVICE_OUT_AUX_DIGITAL = 0x400,
         DEVICE_OUT_ANLG_DOCK_HEADSET = 0x800,
         DEVICE_OUT_DGTL_DOCK_HEADSET = 0x1000,
-#ifdef QCOM_HARDWARE
+#ifdef LEGACY_ALSA_AUDIO
         DEVICE_OUT_USB_ACCESSORY = 0x2000,
         DEVICE_OUT_USB_DEVICE = 0x4000,
-        DEVICE_OUT_FM = 0x8000,
-        DEVICE_OUT_FM_TX = 0x10000,
+        DEVICE_OUT_FM = 0x10000,
         DEVICE_OUT_ANC_HEADSET = 0x20000,
         DEVICE_OUT_ANC_HEADPHONE = 0x40000,
         DEVICE_OUT_PROXY = 0x80000,
@@ -241,22 +235,20 @@ public:
                 DEVICE_OUT_BLUETOOTH_SCO_CARKIT | DEVICE_OUT_BLUETOOTH_A2DP | DEVICE_OUT_BLUETOOTH_A2DP_HEADPHONES |
                 DEVICE_OUT_BLUETOOTH_A2DP_SPEAKER | DEVICE_OUT_AUX_DIGITAL |
                 DEVICE_OUT_ANLG_DOCK_HEADSET | DEVICE_OUT_DGTL_DOCK_HEADSET |
-#ifdef QCOM_HARDWARE
+#ifdef LEGACY_ALSA_AUDIO
                 DEVICE_OUT_USB_ACCESSORY | DEVICE_OUT_USB_DEVICE |
                 DEVICE_OUT_ANC_HEADSET | DEVICE_OUT_ANC_HEADPHONE |
-                DEVICE_OUT_FM | DEVICE_OUT_FM_TX |
-                DEVICE_OUT_PROXY | DEVICE_OUT_DEFAULT),
+                DEVICE_OUT_FM | DEVICE_OUT_PROXY | DEVICE_OUT_DEFAULT),
 #else
                 DEVICE_OUT_DEFAULT),
 #endif
         DEVICE_OUT_ALL_A2DP = (DEVICE_OUT_BLUETOOTH_A2DP | DEVICE_OUT_BLUETOOTH_A2DP_HEADPHONES |
                 DEVICE_OUT_BLUETOOTH_A2DP_SPEAKER),
-#ifdef QCOM_HARDWARE
+#ifdef LEGACY_ALSA_AUDIO
         DEVICE_OUT_ALL_USB = (DEVICE_OUT_USB_ACCESSORY | DEVICE_OUT_USB_DEVICE),
 #endif
 
-        // input devices
-#ifdef QCOM_HARDWARE
+#ifdef LEGACY_ALSA_AUDIO
         DEVICE_IN_COMMUNICATION = 0x100000,
         DEVICE_IN_AMBIENT = 0x200000,
         DEVICE_IN_BUILTIN_MIC = 0x400000,
@@ -266,8 +258,7 @@ public:
         DEVICE_IN_VOICE_CALL = 0x4000000,
         DEVICE_IN_BACK_MIC = 0x8000000,
         DEVICE_IN_ANC_HEADSET = 0x10000000,
-        DEVICE_IN_FM_RX = 0x20000000,
-        DEVICE_IN_FM_RX_A2DP = 0x40000000,
+        DEVICE_IN_FM_TUNER = 0x20000000,
         DEVICE_IN_DEFAULT = DEVICE_IN_BUILTIN_MIC,
         DEVICE_IN_ANLG_DOCK_HEADSET = 0x80000000,
         DEVICE_IN_PROXY = DEVICE_IN_ANLG_DOCK_HEADSET,
@@ -280,22 +271,16 @@ public:
         DEVICE_IN_AUX_DIGITAL = 0x200000,
         DEVICE_IN_VOICE_CALL = 0x400000,
         DEVICE_IN_BACK_MIC = 0x800000,
-#ifdef MTK_HARDWARE
-        DEVICE_IN_FM = 0x20000000,
-#endif
         DEVICE_IN_DEFAULT = 0x80000000,
 #endif
 
         DEVICE_IN_ALL = (DEVICE_IN_COMMUNICATION | DEVICE_IN_AMBIENT | DEVICE_IN_BUILTIN_MIC |
                 DEVICE_IN_BLUETOOTH_SCO_HEADSET | DEVICE_IN_WIRED_HEADSET | DEVICE_IN_AUX_DIGITAL |
-#ifdef QCOM_HARDWARE
+#ifdef LEGACY_ALSA_AUDIO
                 DEVICE_IN_VOICE_CALL | DEVICE_IN_BACK_MIC | DEVICE_IN_ANC_HEADSET |
-                DEVICE_IN_FM_RX | DEVICE_IN_FM_RX_A2DP | DEVICE_IN_DEFAULT |
+                DEVICE_IN_FM_TUNER | DEVICE_IN_DEFAULT |
                 DEVICE_IN_ANLG_DOCK_HEADSET | DEVICE_IN_PROXY)
 #else
-#ifdef MTK_HARDWARE
-                DEVICE_IN_FM |
-#endif
                 DEVICE_IN_VOICE_CALL | DEVICE_IN_BACK_MIC | DEVICE_IN_DEFAULT)
 #endif
     };
@@ -337,13 +322,10 @@ public:
         NUM_DEVICE_STATES
     };
 
-#endif
-
     static uint32_t popCount(uint32_t u) {
         return popcount(u);
     }
 
-#if 1
     static bool isOutputDevice(audio_devices device) {
         if ((popcount(device) == 1) && ((device & ~DEVICE_OUT_ALL) == 0))
              return true;
@@ -377,8 +359,6 @@ public:
     static bool isInputChannel(uint32_t channel) {
         return audio_is_input_channel(channel);
     }
-
-#endif
 };
 
 };
